5a413b84698fdefbb1bf7954234215dd156847c9
Adrakiel <adrakiel@gmail.com>
2018-12-11 16:45:36 -0600

    Login automated

--- core/start.js
+++ core/start.js
@@ -2,14 +2,16 @@ const puppeteer = require('../node_modules/puppeteer');
 const facebook = 'https://facebook.com';
 const sets = {
 	'user': {
-		'mail': 'test@mail.com',
-		'pass': 'test123.'
+		'mail': 'ismaelme@live.com',
+		'pass': 'HolaFaceBook13#'
 	},
 	'sel': {
-		'mail': '#email'
+		'mail': '#email',
+		'pass': '#pass',
+		'log': '#u_0_2'
 	}
 };
-	
+
 let scrape = async () => {
     const firefox = 'firefox';
     const chrome = '/usr/bin/google-chrome-stable';
@@ -23,12 +25,15 @@ let scrape = async () => {
         await page.setViewport({ width: 960, height: 800 });
         console.info('facebook site accessed');
         await page.goto(facebook);
-        await page.waitFor(1000);
+        await page.waitFor(5000);
         await page.click(sets.sel.mail);
         await page.type(sets.sel.mail, sets.user.mail, { delay: 1 });
-        
+        await page.type(sets.sel.pass, sets.user.pass, { delay: 1 });
+	await page.click(sets.sel.log);
+
+
         res = sets;
-     
+
         console.info('Ending App');
 
         return res;
@@ -38,7 +43,7 @@ let scrape = async () => {
     } finally {
         console.info('closing');
         await page.waitFor(20000);
-        browser.close();
+        //browser.close();
         return res;
     }
 
